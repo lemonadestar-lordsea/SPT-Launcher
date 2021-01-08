2398b2b357ca66066f31fde7c4169c936d5610a2
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-01-08 15:44:43 +0100

    Work dump

--- project/Aki.Launcher/Controllers/PatchManager.cs
+++ project/Aki.Launcher/Controllers/PatchManager.cs
@@ -10,25 +10,62 @@
 using System;
 using System.IO;
 using Aki.ByteBanger;
-using Aki.Launcher.Helpers;
 
 namespace Aki.Launcher
 {
 	public static class PatchManager
 	{
-        public static bool ApplyPatches()
+        public static int ApplyPatches(string filepath)
         {
-            string filepath = LauncherSettingsProvider.Instance.GamePath ?? Environment.CurrentDirectory;
-            string targetFile = $@"{filepath}EscapeFromTarkov_Data/Managed/Assembly-CSharp.dll";
-            string patchFile = $@"{filepath}Aki_Data/Launcher/Patches/aki-core.bpf";
+            var patches = RequestPatches();
+
+            // get patches from server
+            if (patches == null)
+            {
+                return -1;
+            }
+
+            // patch from clean files
+            RestorePatched(filepath);
+
+            // apply core patch
+            if (!ApplyCorePatch(filepath))
+            {
+                return -2;
+            }
+
+            // apply mod patches
+            /*
+            for (var patch in patches)
+            {
+                if (!PatchFile(filepath))
+                {
+                    return -3;
+                }
+            }
+            */
+
+            return 1;
+        }
+
+        static PatchInfo[] RequestPatches()
+        {
+            // TODO: get patch files
+            return new PatchInfo[1];
+        }
+
+        static bool ApplyCorePatch(string filepath)
+        {
+            var targetFile = $@"{filepath}EscapeFromTarkov_Data/Managed/Assembly-CSharp.dll";
+            var patchFile = $@"{filepath}Aki_Data/Launcher/Patches/aki-core.bpf";
 
             return PatchFile(targetFile, patchFile);
         }
 
-        public static bool PatchFile(string targetFile, string patchFile)
+        static bool PatchFile(string targetFile, string patchFile)
         {            
-            byte[] target = File.ReadAllBytes(targetFile);
-            byte[] patch = File.ReadAllBytes(patchFile);
+            var target = File.ReadAllBytes(targetFile);
+            var patch = File.ReadAllBytes(patchFile);
             byte[] patched = null;
 
             // backup before patching
@@ -61,6 +98,41 @@ namespace Aki.Launcher
             // apply patch
             File.WriteAllBytes(targetFile, patched);
             return true;
+        }
+
+        public static void RestorePatched(string filepath)
+        {
+            RestorePatchedRecurse(new DirectoryInfo(filepath));
+        }
+
+        static void RestorePatchedRecurse(DirectoryInfo basedir)
+        {
+            // scan subdirectories
+            foreach (var dir in basedir.EnumerateDirectories())
+            {
+                RestorePatchedRecurse(dir);
+            }
+
+            // scan files
+            var files = basedir.GetFiles();
+
+            foreach (var file in files)
+            {
+                if (file.Extension == ".bak")
+                {
+                    var target = Path.ChangeExtension(file.FullName, null);
+
+                    // remove patched file
+                    var patched = new FileInfo(target);
+                    patched.IsReadOnly = false;
+                    patched.Delete();
+
+                    // restore from backup
+                    File.Copy(file.FullName, target);
+                    file.IsReadOnly = false;
+                    file.Delete();
+                }
+            }
         }
     }
 }
