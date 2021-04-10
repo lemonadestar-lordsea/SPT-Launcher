b368c6e3dab1bcf2ccfdac7b24e51e5d0b16f4dc
waffle.lord <waffle.lord@hotmail.com>
2021-04-10 11:05:10 -0400

    update profile every 60 secs while game is running

--- project/Aki.Launcher/DebugWindow.xaml.cs
+++ project/Aki.Launcher/DebugWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace Aki.Launcher
 {
@@ -10,6 +11,34 @@ namespace Aki.Launcher
         public MiniDebugWindow()
         {
             InitializeComponent();
+        }
+
+
+        //I'm lazy and just ripped this from here: https://stackoverflow.com/a/19315242
+        private bool AutoScroll = true;
+        private void sv_ScrollChanged(object sender, System.Windows.Controls.ScrollChangedEventArgs e)
+        {
+            // User scroll event : set or unset auto-scroll mode
+            if (e.ExtentHeightChange == 0)
+            {   // Content unchanged : user scroll event
+                if (sv.VerticalOffset == sv.ScrollableHeight)
+                {   // Scroll bar is in bottom
+                    // Set auto-scroll mode
+                    AutoScroll = true;
+                }
+                else
+                {   // Scroll bar isn't in bottom
+                    // Unset auto-scroll mode
+                    AutoScroll = false;
+                }
+            }
+
+            // Content scroll event : auto-scroll eventually
+            if (AutoScroll && e.ExtentHeightChange != 0)
+            {   // Content changed and auto-scroll mode set
+                // Autoscroll
+                sv.ScrollToVerticalOffset(sv.ExtentHeight);
+            }
         }
     }
 }
