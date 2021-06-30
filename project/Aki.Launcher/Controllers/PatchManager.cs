f026ce92de8212a8242d9b4dab73eeb194caa1cd
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2021-06-30 23:51:05 +0200

    Rework patcher

--- project/Aki.Launcher/Controllers/PatchManager.cs
+++ project/Aki.Launcher/Controllers/PatchManager.cs
@@ -6,9 +6,9 @@
  * Merijn Hendriks
  */
 
-
-using Aki.ByteBanger;
 using System.IO;
+using Aki.Launcher.Helpers;
+using Aki.Launcher.MiniCommon;
 
 namespace Aki.Launcher
 {
@@ -16,57 +16,20 @@ namespace Aki.Launcher
     {
         public static bool ApplyPatches(string filepath)
         {
-            var targetfile = $@"{filepath}EscapeFromTarkov_Data/Managed/Assembly-CSharp.dll";
-            var files = new DirectoryInfo($@"{filepath}Aki_Data/Launcher/Patches/").GetFiles();
+            var modsPath = VFS.Combine(filepath, "Aki_Data/Launcher/Patches/");
+            var mods = VFS.GetDirectories(modsPath);
 
-            foreach (var file in files)
+            RestorePatched(filepath);
+
+            foreach (var mod in mods)
             {
-                // patch from clean files
-                RestorePatched(filepath);
-
-                // apply patch
-                if (ApplyPatch(targetfile, file.FullName))
+                if (!FilePatcher.Run(filepath, VFS.Combine(modsPath, mod)))
                 {
-                    // game patch found
-                    return true;
+                    return false;
                 }
             }
 
-            // patch failed
-            return false;
-        }
-
-        static bool ApplyPatch(string targetfile, string patchfile)
-        {
-            byte[] target = File.ReadAllBytes(targetfile);
-            byte[] patch = File.ReadAllBytes(patchfile);
-
-            // backup before patching
-            if (!File.Exists($@"{targetfile}.bak"))
-            {
-                File.Copy(targetfile, $@"{targetfile}.bak");
-            }
-
-            // patch
-            PatchResult result = PatchUtil.Patch(target, PatchInfo.FromBytes(patch));
-
-            switch (result.Result)
-            {
-                case PatchResultType.Success:                   // successfully patched, write the file
-                    File.WriteAllBytes(targetfile, result.PatchedData);
-                    return true;
-
-                case PatchResultType.AlreadyPatched:            // input file is already patched (size & hash match)
-                case PatchResultType.InputChecksumMismatch:     // input file's hash mismatches
-                case PatchResultType.InputLengthMismatch:       // input file's length mismatches
-                    return true;
-
-                case PatchResultType.OutputChecksumMismatch:    // patched hash mismatches intended output hash
-                    return false;
-
-                default:                                        // something else that shouldn't happen
-                    return false;
-            }
+            return true;
         }
 
         public static void RestorePatched(string filepath)
---
+++ project/Aki.Launcher/Helpers/FilePatcher.cs
@@ -0,0 +1,97 @@
+﻿using System.IO;
+using Aki.ByteBanger;
+using Aki.Launcher.MiniCommon;
+
+namespace Aki.Launcher.Helpers
+{
+    public static class FilePatcher
+    {
+        public static bool Patch(string targetfile, string patchfile)
+        {
+            byte[] target = VFS.ReadFile(targetfile);
+            byte[] patch = VFS.ReadFile(patchfile);
+
+            PatchResult result = PatchUtil.Patch(target, PatchInfo.FromBytes(patch));
+
+            switch (result.Result)
+            {
+                case PatchResultType.Success:
+                    VFS.WriteFile(targetfile, result.PatchedData);
+                    return true;
+
+                case PatchResultType.AlreadyPatched:
+                case PatchResultType.InputChecksumMismatch:
+                case PatchResultType.InputLengthMismatch:
+                    return true;
+
+                case PatchResultType.OutputChecksumMismatch:
+                default:
+                    return false;
+            }
+        }
+
+        private static bool PatchAll(string targetpath, string patchpath)
+        {
+            DirectoryInfo di = new DirectoryInfo(patchpath);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                FileInfo target;
+
+                switch (file.Extension)
+                {
+                    // patch
+                    case ".bpf":
+                        {
+                            target = new FileInfo(VFS.Combine(targetpath, file.Name.Replace(".bpf", "")));
+
+                            if (!Patch(target.FullName, file.FullName))
+                            {
+                                // patch failed
+                                return false;
+                            }
+                        }
+                        break;
+
+                    // add new files
+                    case ".new":
+                        {
+                            target = new FileInfo(VFS.Combine(targetpath, file.Name.Replace(".new", "")));
+                            VFS.WriteFile(target.FullName, Zlib.Decompress(VFS.ReadFile(file.FullName)));
+                        }
+                        break;
+
+                    // delete old files
+                    case ".del":
+                        {
+                            target = new FileInfo(VFS.Combine(targetpath, file.Name.Replace(".del", "")));
+                            target.IsReadOnly = false;
+                            target.Delete();
+                        }
+                        break;
+                }
+
+            }
+
+            foreach (DirectoryInfo directory in di.GetDirectories())
+            {
+                PatchAll(VFS.Combine(targetpath, directory.Name), directory.FullName);
+            }
+
+            di.Refresh();
+
+            if (di.GetFiles().Length == 0 && di.GetDirectories().Length == 0)
+            {
+                // remove empty folders
+                di.Delete();
+            }
+
+            return true;
+        }
+
+        public static bool Run(string targetPath, string patchPath)
+        {
+            return PatchAll(targetPath, patchPath);
+        }
+    }
+}
