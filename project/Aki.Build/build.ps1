a800d0d7d35078ff2cb38b0bd1dd708fdd13e54d
Terkoiz <terkoiz@sp-tarkov.com>
2022-01-11 22:00:31 +0200

    Adjusted code based on review

--- project/Aki.Build/build.ps1
+++ project/Aki.Build/build.ps1
@@ -89,21 +89,21 @@ Write-Host "`nCopying Aki_Data folder ... " -NoNewLine
 
 Copy-Item -Path $launcherData -Destination "./${buildDir}/Aki_Data" -Recurse -Force -ErrorAction SilentlyContinue
 
-Write-Host "`nCopying license file ... " -NoNewLine
-
-$LicenseFilePath = "$($buildDir)/../../LICENSE.md"
-if (Test-Path $LicenseFilePath)
-{
-    Copy-Item -Path $LicenseFilePath -Destination "$($buildDir)/LICENSE-Launcher.txt" -Force -ErrorAction SilentlyContinue
-}
-else
-{
-    Write-Warning "LICENSE.md file not found. If you're making a release, please don't forget to include the license file!"
-}
-
 if (Test-Path "$($buildDir)/Aki_Data") 
 {
     Write-host "OK" -ForegroundColor Green
+    
+	#Additionally copy the license file into the build folder
+	Write-Host "`nCopying license file ... "
+    $LicenseFilePath = "$($buildDir)/../../LICENSE.md"
+    if (Test-Path $LicenseFilePath)
+    {
+        Copy-Item -Path $LicenseFilePath -Destination "$($buildDir)/LICENSE-Launcher.txt" -Force -ErrorAction SilentlyContinue
+    }
+    else
+    {
+        Write-Warning "LICENSE.md file not found. If you're making a release, please don't forget to include the license file!"
+    }
 }
 else 
 {
