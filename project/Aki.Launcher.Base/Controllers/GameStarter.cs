8a055999f6945a194e9e274f319a7bcbee5f0531
Merijn Hendriks <merijn.d.hendriks@gmail.com>
2022-04-10 21:38:29 +0200

    Remove LauncherCLI from IsInstalledInLive

--- project/Aki.Launcher.Base/Controllers/GameStarter.cs
+++ project/Aki.Launcher.Base/Controllers/GameStarter.cs
@@ -122,7 +122,6 @@ namespace Aki.Launcher
                 {
                     // aki files
                     new FileInfo(Path.Combine(_originalGamePath, @"Aki.Launcher.exe")),
-                    new FileInfo(Path.Combine(_originalGamePath, @"LauncherCLI.exe")),
                     new FileInfo(Path.Combine(_originalGamePath, @"Server.exe")),
                     new FileInfo(Path.Combine(_originalGamePath, @"EscapeFromTarkov_Data\Managed\Aki.Build.dll")),
                     new FileInfo(Path.Combine(_originalGamePath, @"EscapeFromTarkov_Data\Managed\Aki.Common.dll")),
